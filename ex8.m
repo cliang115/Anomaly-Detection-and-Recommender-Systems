fa8c753cef6988f9e2a93b62afd6c8e427da954b
Chen <40620795+cliang115@users.noreply.github.com>
2020-02-16 12:00:47 -0800

    Update ex8.m

--- ex8.m
+++ ex8.m
@@ -3,7 +3,12 @@
 %
 %  Instructions
 %  ------------
-%
+% This file contains code that helps you get started on the
+ %  exercise. You will need to complete the following functions:
+ %
+ %     estimateGaussian.m
+ %     selectThreshold.m
+ %     cofiCostFunc.m
 
 %% Initialization
 clear ; close all; clc
